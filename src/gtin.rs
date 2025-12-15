11a1fe3b97a8a364b22092240e2976d92714238b
areif-dev <aj@ajreifsnyder.com>
2025-12-15 15:33:51 -0500

    improve unit tests

--- src/gtin.rs
+++ src/gtin.rs
@@ -339,159 +339,271 @@ impl<'de> Deserialize<'de> for Gtin {
 
 #[cfg(test)]
 mod tests {
-    use crate::{Gtin, GtinError, gtin::calculate_check_digit};
+    use serde::Deserialize;
+    use std::{collections::HashMap, str::FromStr};
 
-    #[test]
-    fn test_serialize() {
-        let upc = Gtin::new("041303015070").unwrap();
-        let json = serde_json::json!({"upc": upc});
-        assert_eq!(json.to_string(), r#"{"upc":"0041303015070"}"#);
-    }
+    use crate::{
+        Gtin, GtinError,
+        gtin::{GtinKind, calculate_check_digit},
+    };
 
-    #[test]
-    fn test_deserialize() {
-        let deserialized: Gtin = serde_json::from_str("\"041303015070\"").unwrap();
-        assert_eq!(deserialized, Gtin::new("041303015070").unwrap());
-    }
+    fn sample_gtins() -> HashMap<GtinKind, Vec<(Gtin, String)>> {
+        let map = HashMap::from([
+            (
+                GtinKind::Gtin12,
+                vec![
+                    (
+                        Gtin::new("082657543338").unwrap(),
+                        String::from("082657543338"),
+                    ),
+                    (
+                        Gtin::new("0037083062113").unwrap(),
+                        String::from("0037083062113"),
+                    ),
+                    (
+                        Gtin::new("037083062311").unwrap(),
+                        String::from("037083062311"),
+                    ),
+                    (
+                        Gtin::new("00037083063196").unwrap(),
+                        String::from("00037083063196"),
+                    ),
+                    (
+                        Gtin::new("0041540303763").unwrap(),
+                        String::from("0041540303763"),
+                    ),
+                    (
+                        Gtin::new("032076938130").unwrap(),
+                        String::from("032076938130"),
+                    ),
+                    (
+                        Gtin::new("00032076938147").unwrap(),
+                        String::from("00032076938147"),
+                    ),
+                    (
+                        Gtin::new("795711340001").unwrap(),
+                        String::from("795711340001"),
+                    ),
+                ],
+            ),
+            (
+                GtinKind::Gtin13,
+                vec![
+                    (
+                        Gtin::new("4059433692142").unwrap(),
+                        String::from("4059433692142"),
+                    ),
+                    (
+                        Gtin::new("5038083603370").unwrap(),
+                        String::from("5038083603370"),
+                    ),
+                    (
+                        Gtin::new("05051771685816").unwrap(),
+                        String::from("05051771685816"),
+                    ),
+                    (
+                        Gtin::new("04059433537863").unwrap(),
+                        String::from("04059433537863"),
+                    ),
+                    (
+                        Gtin::new("4055744029608").unwrap(),
+                        String::from("4055744029608"),
+                    ),
+                    (
+                        Gtin::new("04055744029615").unwrap(),
+                        String::from("04055744029615"),
+                    ),
+                    (
+                        Gtin::new("9340710002422").unwrap(),
+                        String::from("9340710002422"),
+                    ),
+                    (
+                        Gtin::new("09340710002484").unwrap(),
+                        String::from("09340710002484"),
+                    ),
+                ],
+            ),
+            (
+                GtinKind::Gtin8,
+                vec![
+                    (Gtin::new("12345670").unwrap(), String::from("12345670")),
+                    (Gtin::new("76543210").unwrap(), String::from("76543210")),
+                    (Gtin::new("32214987").unwrap(), String::from("32214987")),
+                    (Gtin::new("65498712").unwrap(), String::from("65498712")),
+                    (Gtin::new("15607652").unwrap(), String::from("15607652")),
+                    (Gtin::new("16784093").unwrap(), String::from("16784093")),
+                    (Gtin::new("50287895").unwrap(), String::from("50287895")),
+                    (Gtin::new("49215403").unwrap(), String::from("49215403")),
+                ],
+            ),
+            (
+                GtinKind::Gtin14,
+                vec![
+                    (
+                        Gtin::new("10082657543335").unwrap(),
+                        String::from("10082657543335"),
+                    ),
+                    (
+                        Gtin::new("30037083063197").unwrap(),
+                        String::from("30037083063197"),
+                    ),
+                    (
+                        Gtin::new("10037083062110").unwrap(),
+                        String::from("10037083062110"),
+                    ),
+                    (
+                        Gtin::new("20037083062315").unwrap(),
+                        String::from("20037083062315"),
+                    ),
+                    (
+                        Gtin::new("50070257509583").unwrap(),
+                        String::from("50070257509583"),
+                    ),
+                    (
+                        Gtin::new("60041540303765").unwrap(),
+                        String::from("60041540303765"),
+                    ),
+                    (
+                        Gtin::new("10795711340008").unwrap(),
+                        String::from("10795711340008"),
+                    ),
+                    (
+                        Gtin::new("14055744029605").unwrap(),
+                        String::from("14055744029605"),
+                    ),
+                ],
+            ),
+        ]);
 
-    #[test]
-    fn test_display() {
-        assert_eq!(
-            format!("{}", Gtin::new("041303015070").unwrap()),
-            "GTIN(0041303015070)"
-        );
-    }
-
-    #[test]
-    fn test_to_string() {
-        assert_eq!(
-            Gtin::new("0041303015070").unwrap().to_string(),
-            "0041303015070".to_string()
-        );
+        for (kind, list) in map.iter() {
+            for (gtin, code) in list {
+                assert_eq!(*kind, gtin.kind());
+            }
+        }
+        map
     }
 
     #[test]
     fn test_calculate_check_digit() {
-        let valids = [
-            ("004130301507", 0u8),
-            ("007797509512", 6),
-            ("073803920906", 3),
-            ("001410005297", 5),
-            ("080882908146", 6),
-            ("080812411166", 0),
-            ("934071000250", 7),
-            ("934071000251", 4),
-            ("934071000241", 5),
-        ];
-
-        for (upc, check) in valids {
-            let first_12: [u8; 12] = upc
-                .chars()
-                .map(|c| c.to_digit(10).unwrap() as u8)
-                .collect::<Vec<u8>>()
-                .try_into()
-                .unwrap();
-            assert_eq!(calculate_check_digit(first_12), check);
+        let samples = sample_gtins();
+        for (_, gtins_and_codes) in samples {
+            for (_, code) in gtins_and_codes {
+                let mut digits = [0u8; 13];
+                for (i, char) in code.chars().rev().skip(1).enumerate() {
+                    digits[12 - i] = char.to_digit(10).unwrap().try_into().unwrap();
+                }
+                let expected_check_digit = calculate_check_digit(digits);
+                let actual_check_digit: u8 = code
+                    .chars()
+                    .last()
+                    .unwrap()
+                    .to_digit(10)
+                    .unwrap()
+                    .try_into()
+                    .unwrap();
+                assert_eq!(expected_check_digit, actual_check_digit);
+            }
         }
     }
 
     #[test]
     fn test_new() {
-        assert!(Gtin::new("0000000000000").is_ok());
-        assert!(Gtin::new("9340710002507").is_ok());
-        assert!(Gtin::new("9340710002415").is_ok());
-        assert!(Gtin::new("9340710002514").is_ok());
+        let _ = sample_gtins();
         assert_eq!(Gtin::new(""), Err(GtinError::InvalidLength));
         assert_eq!(Gtin::new("041303015071"), Err(GtinError::InvalidCheckDigit));
         assert_eq!(
             Gtin::new("0041303015071"),
             Err(GtinError::InvalidCheckDigit)
         );
-        assert!(Gtin::new("0703948501157").is_ok());
-        assert_eq!(Gtin::new("00413b3015071"), Err(GtinError::InvalidDigit));
-    }
-
-    #[test]
-    fn test_from_str_nonstrict() {
-        // All non-numeric characters are ignored
-        assert_eq!(
-            Gtin::from_str_nonstrict("a761458256240").unwrap(),
-            Gtin::new("0761458256240").unwrap()
-        );
-
-        // Codes shorter than 13 digits will be forward padded with zeros until their length is 13
-        assert_eq!(
-            Gtin::from_str_nonstrict("1234565").unwrap(),
-            Gtin::new("0000001234565").unwrap()
-        );
-
-        // Codes longer than 13 characters without any preceeding zeros will still fail to generate
-        assert_eq!(
-            Gtin::from_str_nonstrict("12345678901234"),
-            Err(GtinError::InvalidLength)
-        );
-
-        // Codes longer than 13 digits with preceeding zeros will have preceeding zeros truncated
-        assert_eq!(
-            Gtin::from_str_nonstrict("00000000001234565").unwrap(),
-            Gtin::new("0000001234565").unwrap()
-        );
-
-        // Codes with an incorrect check digit will have their check digit fixed
-        assert_eq!(
-            Gtin::from_str_nonstrict("0000001234566").unwrap(),
-            Gtin::new("0000001234565").unwrap()
-        );
-
-        // Be very careful! Strings that are not close to barcodes at all may still result in valid
-        // looking [`Gtin`]s
-        assert_eq!(
-            Gtin::from_str_nonstrict("absolute nonsense").unwrap(),
-            Gtin::new("0000000000000").unwrap()
-        );
-
-        assert_eq!(
-            Gtin::from_str_nonstrict("9340710002515").unwrap(),
-            Gtin::new("9340710002514").unwrap()
-        );
-        assert_eq!(
-            Gtin::from_str_nonstrict("9340710002507").unwrap(),
-            Gtin::new("9340710002507").unwrap()
-        );
-        assert_eq!(
-            Gtin::from_str_nonstrict("9340710002415").unwrap(),
-            Gtin::new("9340710002415").unwrap()
-        );
+        assert_eq!(Gtin::new("00413b3015071"), Err(GtinError::InvalidCharacter));
+        assert_eq!(Gtin::new("123456789012345"), Err(GtinError::InvalidLength));
     }
 
     #[test]
     fn test_as_arr() {
-        assert_eq!(
-            Gtin::new("706285102001").unwrap().as_arr(),
-            [0, 7, 0, 6, 2, 8, 5, 1, 0, 2, 0, 0, 1]
-        );
+        let samples = sample_gtins();
+        for (_, gtins_and_codes) in samples {
+            for (gtin, code) in gtins_and_codes {
+                let mut digits_from_code = [0u8; 14];
+                for (i, char) in code.chars().enumerate() {
+                    digits_from_code[i + (14 - code.len())] =
+                        char.to_digit(10).unwrap().try_into().unwrap();
+                }
+                assert_eq!(gtin.as_arr(), digits_from_code);
+            }
+        }
     }
 
     #[test]
-    fn test_is_upca() {
-        assert!(Gtin::new("0041303015070").unwrap().is_upca());
-        assert!(!Gtin::new("6936685222021").unwrap().is_upca());
+    fn test_normalize_digits() {
+        let samples = sample_gtins();
+        for (kind, gtins_and_codes) in samples {
+            for (gtin, _) in gtins_and_codes {
+                let expected_length = match kind {
+                    GtinKind::Gtin12 => 12,
+                    GtinKind::Gtin8 => 8,
+                    GtinKind::Gtin13 => 13,
+                    GtinKind::Gtin14 => 14,
+                };
+                assert_eq!(gtin.normalize_digits().len(), expected_length);
+            }
+        }
     }
 
     #[test]
-    fn test_to_upca_string() {
-        assert_eq!(
-            Gtin::new("0041303015070").unwrap().normalize_digits(),
-            "041303015070".to_string()
-        );
-        assert_eq!(
-            Gtin::new("6936685222021").unwrap().normalize_digits(),
-            "6936685222021".to_string()
-        );
+    fn test_display_gtin() {
+        let samples = sample_gtins();
+        for (_kind, gtins_and_codes) in samples {
+            for (gtin, _code) in gtins_and_codes {
+                assert_eq!(
+                    gtin.to_string(),
+                    gtin.as_arr()
+                        .iter()
+                        .map(|d| d.to_string())
+                        .collect::<String>()
+                );
+            }
+        }
     }
 
     #[test]
-    fn test_default() {
-        assert_eq!(Gtin::default(), Gtin::new("0000000000000").unwrap());
+    fn test_from_str_gtin() {
+        let samples = sample_gtins();
+        for (_kind, gtins_and_codes) in samples {
+            for (gtin, code) in gtins_and_codes {
+                assert_eq!(Gtin::from_str(&code).unwrap(), gtin);
+            }
+        }
+    }
+
+    #[test]
+    fn test_serialize_gtin() {
+        let samples = sample_gtins();
+        for (_kind, gtins_and_codes) in samples {
+            for (gtin, _code) in gtins_and_codes {
+                let json = serde_json::json!({
+                    "barcode": gtin,
+                });
+                let code = gtin.to_string();
+                assert_eq!(json.to_string(), format!("{{\"barcode\":\"{}\"}}", code));
+            }
+        }
+    }
+
+    #[derive(Deserialize, PartialEq, Eq, Debug)]
+    struct TestContainer {
+        barcode: Gtin,
+    }
+
+    #[test]
+    fn test_deserialize_gtin() {
+        let samples = sample_gtins();
+        for (_kind, gtins_and_codes) in samples {
+            for (gtin, _code) in gtins_and_codes {
+                let json = serde_json::json!({"barcode": gtin});
+                let container: TestContainer = serde_json::from_value(json).unwrap();
+                assert_eq!(container, TestContainer { barcode: gtin });
+            }
+        }
     }
 }
